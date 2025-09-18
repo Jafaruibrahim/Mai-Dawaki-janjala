d35185f4080758eed9ee82b1facf1a927f9370c5
Jafaru <155572478+Jafaruibrahim@users.noreply.github.com>
2025-09-18 10:08:07 +0100

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,34 @@
+#conditional satement
+print("Menu:")
+print("1. Rice")
+print("2. Beans")
+print("3. Yam")
+print("4. Banana")
+
+choice = int(input("Enter your choice (1-4): "))
+
+if choice == 1:
+    print("You chose Rice 🍚")
+elif choice == 2:
+    print("You chose Beans 🥘")
+elif choice == 3:
+    print("You chose Yam 🍠")
+elif choice == 4:
+    print("You chose Banana")    
+else:
+    print("Invalid choice ❌")
+    
+choice = int(input("Enter a number (1 or 2): "))
+#second
+if choice == 1:
+    print("You chose option 1")
+elif choice == 2:
+    print("You chose option 2")
+else:
+    print("Invalid choice")    
+    
+    
+    
+    
+    
+
